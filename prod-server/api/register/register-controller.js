cd172e4a3b8d36e250fa367413d1488dd8d31b9d
rscottjohnson <rscottjohnson@acm.org>
2021-02-23 23:25:55 -0500

    Added new user save to register controller

--- prod-server/api/register/register-controller.js
+++ prod-server/api/register/register-controller.js
@@ -7,17 +7,31 @@ exports.index = index;
 
 var _stringUtil = require('../../utilities/string-util');
 
+var _userModel = require('../../model/user-model');
+
+var _userModel2 = _interopRequireDefault(_userModel);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
 function index(req, res) {
   var validation = validateIndex(req.body);
   if (!validation.isValid) {
     return res.status(400).json({ message: validation.message });
   }
-  var user = {
+  var user = new _userModel2.default({
     username: req.body.username.toLowerCase(),
     password: req.body.password
-  };
-  console.log(user);
-  return res.status(201).json();
+  });
+  user.save(function (error) {
+    if (error) {
+      // if the error code is 11000 (mongoose: username taken), return error
+      if (error.code === 11000) {
+        return res.status(403).json({ message: 'Username is already taken' });
+      }
+      return res.status(500).json();
+    }
+    return res.status(201).json();
+  });
 }
 
 function validateIndex(body) {
