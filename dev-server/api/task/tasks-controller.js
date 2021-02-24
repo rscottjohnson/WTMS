2ce9914e3e79567be1e4c59c1689c48d455c48c3
rscottjohnson <rscottjohnson@acm.org>
2021-02-23 22:10:19 -0500

    Added create function

--- dev-server/api/task/tasks-controller.js
+++ dev-server/api/task/tasks-controller.js
@@ -1,3 +1,7 @@
+import User from '../../model/user-model';
+import Task from '../../model/task-model';
+import moment from 'moment';
+
 export function index(req, res) {
   // find all tasks
   return res.status(200).json();
@@ -5,6 +9,30 @@ export function index(req, res) {
 
 export function create(req, res) {
   // create task
+  const id = 10; // fake id to save with the user
+  // find the user by the id, an error or the user will be returned
+  User.findOne({ _id: id }, (error, user) => {
+    // if error is returned or user isn't found
+    if (error && !user) { 
+      return res.status(500).json();
+    }
+    // constant for task
+    // pass in the task object from the request
+    const task = new Task()(req.body.task);
+    // tie the task to the user
+    task.author = user._id;
+    // format the due date
+    task.dueDate = moment(task.dueDate);
+    // save the task to the database
+    task.save(error => {
+      if (error) {
+        return res.status(500).json();
+      }
+      return res.status(201).json();
+    })
+
+  }); 
+  
   return res.status(201).json();
 }
 
--- prod-server/api/task/tasks-controller.js
+++ prod-server/api/task/tasks-controller.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 Object.defineProperty(exports, "__esModule", {
   value: true
@@ -8,6 +8,21 @@ exports.create = create;
 exports.update = update;
 exports.remove = remove;
 exports.show = show;
+
+var _userModel = require('../../model/user-model');
+
+var _userModel2 = _interopRequireDefault(_userModel);
+
+var _taskModel = require('../../model/task-model');
+
+var _taskModel2 = _interopRequireDefault(_taskModel);
+
+var _moment = require('moment');
+
+var _moment2 = _interopRequireDefault(_moment);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
 function index(req, res) {
   // find all tasks
   return res.status(200).json();
@@ -15,6 +30,29 @@ function index(req, res) {
 
 function create(req, res) {
   // create task
+  var id = 10; // fake id to save with the user
+  // find the user by the id, an error or the user will be returned
+  _userModel2.default.findOne({ _id: id }, function (error, user) {
+    // if error is returned or user isn't found
+    if (error && !user) {
+      return res.status(500).json();
+    }
+    // constant for task
+    // pass in the task object from the request
+    var task = new _taskModel2.default()(req.body.task);
+    // tie the task to the user
+    task.author = user._id;
+    // format the due date
+    task.dueDate = (0, _moment2.default)(task.dueDate);
+    // save the task to the database
+    task.save(function (error) {
+      if (error) {
+        return res.status(500).json();
+      }
+      return res.status(201).json();
+    });
+  });
+
   return res.status(201).json();
 }
 
