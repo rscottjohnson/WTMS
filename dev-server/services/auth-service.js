a286eaaba4a646eafa5b3d922333bf4802cfc582
rscottjohnson <rscottjohnson@acm.org>
2021-03-07 17:01:50 -0500

    Added requireLogin function to access certain routes

--- dev-server/services/auth-service.js
+++ dev-server/services/auth-service.js
@@ -6,3 +6,31 @@ export function generateJWT(user) {
   // create the token
   return jwt.sign({ user: tokenData }, process.env.TOKEN_SECRET);
 }
+
+// Require login for any routes that the user needs to be logged in to access
+export function requireLogin(req, res, next) {
+  const token = decodeToken(req);
+  // If there is no token, then return an error letting the user know they must login
+  if (!token) {
+    return res.status(401).json({ message: 'You must be logged in.' });
+  }
+  // Otherwise, we know we have a valid token and the user is logged in
+  next();
+}
+
+// Decrypt the token
+export function decodeToken(req) {
+  // Obtain the token from the headers
+  const token = req.headers.authorization || req.headers ['authorization'];
+
+  // If there is no token, return null, otherwise decrypt the token
+  if (!token) {
+    return null;
+  }
+
+  try {
+    return jwt.verify(token, process.env.TOKEN_SECRET);
+  } catch (error) {
+    return null;
+  }
+}
--- prod-server/services/auth-service.js
+++ prod-server/services/auth-service.js
@@ -4,6 +4,8 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.generateJWT = generateJWT;
+exports.requireLogin = requireLogin;
+exports.decodeToken = decodeToken;
 
 var _jsonwebtoken = require('jsonwebtoken');
 
@@ -16,4 +18,32 @@ function generateJWT(user) {
   var tokenData = { username: user.username, id: user._id };
   // create the token
   return _jsonwebtoken2.default.sign({ user: tokenData }, process.env.TOKEN_SECRET);
+}
+
+// Require login for any routes that the user needs to be logged in to access
+function requireLogin(req, res, next) {
+  var token = decodeToken(req);
+  // If there is no token, then return an error letting the user know they must login
+  if (!token) {
+    return res.status(401).json({ message: 'You must be logged in.' });
+  }
+  // Otherwise, we know we have a valid token and the user is logged in
+  next();
+}
+
+// Decrypt the token
+function decodeToken(req) {
+  // Obtain the token from the headers
+  var token = req.headers.authorization || req.headers['authorization'];
+
+  // If there is no token, return null, otherwise decrypt the token
+  if (!token) {
+    return null;
+  }
+
+  try {
+    return _jsonwebtoken2.default.verify(token, process.env.TOKEN_SECRET);
+  } catch (error) {
+    return null;
+  }
 }
