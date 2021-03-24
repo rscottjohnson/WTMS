9c3afe2d24b83ffb097bd16b06fffa7abff9c9bd
rscottjohnson <rscottjohnson@acm.org>
2021-03-23 23:54:55 -0400

    Add date filter

--- src/main.js
+++ src/main.js
@@ -6,9 +6,18 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'jquery/src/jquery.js'
 import 'bootstrap/dist/js/bootstrap.min.js'
 import BootstrapVue from 'bootstrap-vue'
+import moment from 'moment'
 
 Vue.use(BootstrapVue);
 Vue.config.productionTip = process.env.NODE_ENV === 'production';
+Vue.filter('date', (value) => {
+  // if there is no value, return empty string
+  if (!value) {
+    return '';
+  }
+  // format the date value with moment
+  return moment(value).format('MMM DD, YYYY');
+});
 
 new Vue({
   router,
