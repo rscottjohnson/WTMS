1827b3516385cdd3880d84307449fa9fdaba9e3d
rscottjohnson <rscottjohnson@acm.org>
2021-03-23 23:39:45 -0400

    Relabel is-late to late

--- src/views/tasks/TasksAll.vue
+++ src/views/tasks/TasksAll.vue
@@ -19,7 +19,7 @@
         <div class="card-body">
           <div class="d-flex justify-content-between">
             <h5 class="card-title">{{ task.title }}</h5>
-            <span v-bind:class="{ is-late: taskIsLate(task.dueDate) }" class="small">{{ task.dueDate }}</span>
+            <span v-bind:class="{ late: taskIsLate(task.dueDate) }" class="small">{{ task.dueDate }}</span>
           </div>
           <h6 class="card-subtitle mb-2 text-muted">
             Created by {{ task.author.username }}
