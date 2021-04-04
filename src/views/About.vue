ab397c7db05b6e5f8facc2459f9d7019d636e8ee
rscottjohnson <rscottjohnson@acm.org>
2021-04-04 18:07:10 -0400

    Add text to About page and edit Navbar to include link to route

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <h1 class="display-4">About WTMS</h1>
+        <p class="lead">
+          The Web Task Management System (WTMS) is a web application that a team can utilize to manage tasks as part of a workgroup.  Members can perform the following task actions:
+          <ul>
+            <li>Create</li>
+            <li>Edit</li>
+            <li>Track the Completion Of</li>
+            <li>Delete</li>
+          </ul> 
+          WTMS utilizes MongoDB, ExpressJS, VueJS, and NodeJS.
+        </p>
+      </div>
+    </div>
   </div>
 </template>
